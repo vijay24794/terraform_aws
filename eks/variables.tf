ffb739b2f78db1754608e377b09f8681901af309
vijay24794 <56646134+vijay24794@users.noreply.github.com>
2023-09-23 09:49:33 +0530

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -3,3 +3,14 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "access_key" {
+  description = "AWS access_key"
+  type        = string
+  default     = "AKIAXKRNQ6PFZIIY3Z7N"
+}
+variable "secret_key" {
+  description = "AWS secret_key"
+  type        = string
+  default     = "ZmZB67i26YDJwBx9qgRUnJ+qop0qDO9miU4j66wP"
+}
+
